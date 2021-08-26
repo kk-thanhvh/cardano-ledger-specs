065cc21fc682d84076cc4ae14a38c9bbe6616ef5
Zachary Churchill <zacharyachurchill@gmail.com>
2021-08-26 16:17:15 -0400

    minimal annotatedBlock

--- eras/shelley/impl/src/Cardano/Ledger/Shelley/API/Validation.hs
+++ eras/shelley/impl/src/Cardano/Ledger/Shelley/API/Validation.hs
@@ -19,28 +19,36 @@ module Cardano.Ledger.Shelley.API.Validation
     TickTransitionError (..),
     BlockTransitionError (..),
     chainChecks,
+    annotateBlock,
+    applyBlockOptsAnnotated,
+    AnnotatedBlock (..),
   )
 where
 
-import Cardano.Ledger.BaseTypes (Globals (..), ShelleyBase)
+import Cardano.Ledger.BaseTypes (Globals (..), ShelleyBase, epochInfo)
 import Cardano.Ledger.Core (AnnotatedData, ChainData, SerialisableData)
 import qualified Cardano.Ledger.Core as Core
+import qualified Cardano.Ledger.Crypto as CC
 import Cardano.Ledger.Era (Crypto, Era)
 import Cardano.Ledger.Shelley (ShelleyEra)
 import Cardano.Ledger.Shelley.API.Protocol (PraosCrypto)
-import Cardano.Ledger.Shelley.BlockChain (Block)
-import Cardano.Ledger.Shelley.LedgerState (NewEpochState)
+import Cardano.Ledger.Shelley.BlockChain (Block (..))
+import Cardano.Ledger.Shelley.LedgerState (NewEpochState (..))
 import qualified Cardano.Ledger.Shelley.LedgerState as LedgerState
 import Cardano.Ledger.Shelley.PParams (PParams' (..))
 import qualified Cardano.Ledger.Shelley.Rules.Bbody as STS
 import qualified Cardano.Ledger.Shelley.Rules.Chain as STS
 import Cardano.Ledger.Shelley.Rules.EraMapping ()
-import Cardano.Ledger.Slot (SlotNo)
-import Cardano.Protocol.TPraos.BHeader (BHeader)
+import Cardano.Ledger.Slot (SlotNo (..), epochInfoSize)
+import Cardano.Protocol.TPraos.BHeader (BHBody (..), BHeader (..), bhbody)
+import Cardano.Slotting.EpochInfo (epochInfoFirst, epochInfoSlotToUTCTime)
+import Cardano.Slotting.Slot (EpochNo, EpochSize)
 import Control.Arrow (left, right)
 import Control.Monad.Except
+import Control.Monad.Identity (runIdentity)
 import Control.Monad.Trans.Reader (runReader)
 import Control.State.Transition.Extended
+import Data.Time (UTCTime)
 import GHC.Generics (Generic)
 import NoThunks.Class (NoThunks (..))
 
@@ -258,3 +266,44 @@ deriving stock instance
 instance
   (NoThunks (STS.PredicateFailure (Core.EraRule "BBODY" era))) =>
   NoThunks (BlockTransitionError era)
+
+data AnnotatedBlock = AnnotatedBlock
+  { -- abEra :: !Era
+    abEpochNo :: !EpochNo,
+    abSlotNo :: !SlotNo,
+    abEpochSlot :: !SlotNo, -- The slot within the epoch (starts at 0 for first slot of each epoch
+    abTimeStamp :: !UTCTime, -- The slot number converted to UTCTime
+    abEpochSize :: !EpochSize -- Number of slots in current epoch
+    -- , abTxs :: [AnnotatedTx]   -- All fields in the superset of all block types
+  }
+
+annotateBlock ::
+  (CC.Crypto (Crypto era), Applicative f) =>
+  Globals ->
+  NewEpochState era ->
+  Block era ->
+  f AnnotatedBlock
+annotateBlock globals state _blk@(Block' bheader _ _) = do
+  let slotNo = bheaderSlotNo $ bhbody bheader
+      epochNo = nesEL state
+      ann =
+        AnnotatedBlock
+          { abEpochNo = epochNo,
+            abSlotNo = slotNo,
+            abEpochSlot = runIdentity $ (epochInfoFirst $ epochInfo globals) epochNo,
+            abTimeStamp = runIdentity $ epochInfoSlotToUTCTime (epochInfo globals) (systemStart globals) slotNo,
+            abEpochSize = runReader (epochInfoSize (epochInfo globals) epochNo) globals
+          }
+  pure ann
+
+applyBlockOptsAnnotated ::
+  forall ep m era.
+  (EventReturnTypeRep ep, MonadError (BlockTransitionError era) m, ApplyBlock era, CC.Crypto (Crypto era)) =>
+  ApplySTSOpts ep ->
+  Globals ->
+  NewEpochState era ->
+  Block era ->
+  m (EventReturnType ep (Core.EraRule "BBODY" era) (NewEpochState era), AnnotatedBlock)
+applyBlockOptsAnnotated opts globals state blk = do
+  (,) <$> applyBlockOpts opts globals state blk
+    <*> annotateBlock globals state blk
